70453856cfdc56875b88ad5eebbe8dd9f81f3bb2
Kris O. Gammelgaard <123dkris@gmail.com>
2018-04-10 18:29:35 +0200

    .

--- Bot.py
+++ Bot.py
@@ -19,18 +19,18 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
-    if message.content.startswith('!hello'):
+    elif message.content.startswith('!hello'):
         msg = 'Hello {0.author.mention}'.format(message)
         await bot.send_message(message.channel, msg)
 
-    #turn on/off message spam
-    if message.content.startswith('!turnon'):
+    # turn on/off message spam
+    elif message.content.startswith('!turnon'):
         if message.author.id == '106814905583169536':
             isEnabled = 'on'
             msg = 'Message spam is enabled'
             await bot.send_message(message.channel, msg)
             return
-        if (message.author.id == '106814905583169536') & (isEnabled == 'on'):
+        elif (message.author.id == '106814905583169536') & (isEnabled == 'on'):
             msg = 'Message spam is already enabled'
             await bot.send_message(message.channel, msg)
             return
@@ -38,13 +38,13 @@ async def on_message(message):
             msg = '{0.author.mention} you are not authorised for this command'.format(message)
             await bot.send_message(message.channel, msg)
             return
-    if message.content.startswith('!turnoff'):
+    elif message.content.startswith('!turnoff'):
         if (message.author.id == '106814905583169536') & (isEnabled == 'on'):
             isEnabled = 'off'
             msg = 'Message spam is disabled'
             await bot.send_message(message.channel, msg)
             return
-        if (message.author.id == '106814905583169536') & (isEnabled == 'off') | (message.author.id == '106814905583169536') & (isEnabled == 'dif'):
+        elif (message.author.id == '106814905583169536') & (isEnabled == 'off') | (message.author.id == '106814905583169536') & (isEnabled == 'dif'):
             msg = 'Message spam is already disabled'
             await bot.send_message(message.channel, msg)
             return
@@ -54,16 +54,16 @@ async def on_message(message):
             return
 
 
-
 @bot.event
 async def on_typing(channel, Member, when):
     if (Member.id == '284823748371021825') & (isEnabled == 'on'):
         msg = 'V this is an impostor, execute him V'
         await bot.send_message(channel, msg)
 
-    if (Member.id == '106814905583169536') & (isEnabled == 'on'):
+    elif (Member.id == '106814905583169536') & (isEnabled == 'on'):
         msg = 'SHUT THE FUCK UP, the REAL queen is speaking'
         await bot.send_message(channel, msg)
+
 
 @bot.event
 async def on_ready():
